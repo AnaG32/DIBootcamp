80ff72a789bfde605311b68be70463f5951c8fb5
AnaG32 <anaguttmann5@gmail.com>
2022-06-26 16:38:38 +0300

    files changed

---
+++ WEEK2-Javascript/Day1/Exercises/script.js
@@ -0,0 +1,26 @@
+// Exercise 1 //
+
+let adressStreet = 'Yeelim';
+let adressNumber = '3';
+let country = 'Israel';
+let globalAdress = (adressStreet + ' ' + adressNumber + ' ' + country);
+
+console.log ("I live in  " + globalAdress);
+
+
+// Exercise 2 //
+
+let birth = 1989;
+let future = 2028;
+let result = future - birth;
+
+console.log ("I will be  " + result + "  in " + future);
+
+
+//Exercise 3 //
+
+let animals = ['cat','dog','fish','rabbit','cow'];
+console.log (animals[1]);
+
+animals.push('horse');
+console.log (animals);
