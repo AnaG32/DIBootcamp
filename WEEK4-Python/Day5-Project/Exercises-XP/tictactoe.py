7c05238d6efc090c70956d87a515ef1c72e8a299
AnaG32 <anaguttmann5@gmail.com>
2022-07-17 10:26:48 +0300

    files changed

--- WEEK4-Python/Day5-Project/Exercises-XP/tictactoe.py
+++ WEEK4-Python/Day5-Project/Exercises-XP/tictactoe.py
@@ -5,97 +5,94 @@
 # The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
 # When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.
 
-from ast import While
-import time
-
-board = [
-    " ", " ", " ",
-    " ", " ", " ",
-    " ", " ", " " 
-    ]
+board =[  "-", "-", "-",
+             "-", "-", "-",
+              "-", "-", "-"]
 
 winner = None
 
 def play():
     global winner
+    print("Start Game")
     display_board()
-    
+
     for i in range(5):
-        print("Player 1 - Use X")
-        value = "X"
-        player_input(value) # jugada
-        check_win() # hubo ganardor
-    
-        if winner != "X" and 1 < 4: 
+        print("Turn Player X - Use numbers to choose a position")
+        value="X"
+        
+        player_input(value)
+        check_win()
+        
+        if winner != "X" and i < 4 :
             for j in range(3):
-                print("Player 2 - Use O")
-                value = "O"
+                print("Turn Player O - Use numbers to choose a position")
+                value="O"
                 player_input(value)
                 check_win()
-            
+
                 if winner == "O":
-                    print("Congratulation! Player 2 is the winner")
+                    print("Congratulation! Player O is the winner")
                 break
         elif winner == "X":
-            print("Congratulation! Player 1 is the winner")
+            print("Congratulation! Player X is the winner")
             break
         else:
-            print("Anyone is the winner")
+            print("Anyone is the winner - play again")
+
 
 def check_win():
     global winner
-    row_control()
-    column_control()
-    diagonal_control()
+    row_check()
+    column_check()
+    diagonal_check()
 
-def row_control():
+def row_check():
     global winner
-    if board[0] == board[1] == board[2] != "-":
+    if board[0] == board[1]==board[2] != "-":
         winner = board[0]
-    elif board[3] == board[4] == board[5] != "-":
+    elif board[3] ==  board[4] == board[5] != "-":
         winner = board[3]
-    elif board[6] == board[7] == board[8] != "-":
-        winner[6]
+    elif board[6] ==  board[7] == board[8] != "-":
+        winner = board[6]
 
-def column_control():
+def column_check():
     global winner
-    if board[0] == board[3] == board[6] != "-":
+    if board[0] ==  board[3] == board[6] != "-":
         winner = board[0]
-    elif board[1] == board[4] == board[7] != "-":
+    elif board[1] ==  board[4] == board[7] != "-":
         winner = board[1]
-    elif board[2] == board[5] == board[8] != "-":
+    elif board[2] ==  board[5] == board[8] != "-":
         winner = board[2]
-        
-def diagonal_control():
+
+def diagonal_check():
     global winner
-    if board[0] == board[4] == board[8] != "-":
+    if board[0] ==  board[4] == board[8] != "-":
         winner = board[0]
-    elif board[2] == board[4] == board[6] != "-":
+    elif board[2] ==  board[4] == board[6] != "-":
         winner = board[2]
-        
-        
+
+
 def player_input(value):
-    write_input = False
-    
-    While write_input == False:
-        position = int(input("Choose a position between 1 and 9"))
+    write_down = False
+
+    while write_down == False:
+        position = int(input("Choose a position between 1 and 9:  "))
         position -= 1
-    
-        if table[position] == "-":
-            write_input = True
+
+        if board[position] == "-":
+            write_down = True
         else:
             print("Choose an empty position")
-    
+
     board[position] = value
     display_board()
-        
+
 
 def display_board():
-    
     print("\n")
-    print(board[0] + " | " + board[1] + " | " + board[2] + "   1 | 2 | 3 ")
-    print(board[3] + " | " + board[4] + " | " + board[5] + "   4 | 5 | 6 ")
-    print(board[6] + " | " + board[7] + " | " + board[8] + "   7 | 8 | 9 ")
+    print(board[0] + " | " + board[1] + " | " +  board[2]  +"       1 | 2 | 3")
+    print(board[3] + " | " + board[4] + " | " +  board[5]  +"       4 | 5 | 6")
+    print(board[6] + " | " + board[7] + " | " +  board[8]  +"       7 | 8 | 9")
     print("\n")
 
 play()
